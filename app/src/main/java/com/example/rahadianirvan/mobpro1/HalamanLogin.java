f6ae528c0da43365e60a2a42c6e49cc69a19f515
colep\Rahadian Irvan <colep.ok@gmail.com>
2016-09-29 18:19:00 +0700

    no message

--- app/src/main/java/com/example/rahadianirvan/mobpro1/HalamanLogin.java
+++ app/src/main/java/com/example/rahadianirvan/mobpro1/HalamanLogin.java
@@ -1,13 +1,49 @@
 package com.example.rahadianirvan.mobpro1;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HalamanLogin extends AppCompatActivity {
-
+    Button btnLogin,btnRegister,btnFacebook,btnGmail,btnTwitter;
+    EditText user,pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_login);
+    }
+
+    public void masuk(View view){
+        user = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+            Intent intent = new Intent(this, HalamanMasuk.class);
+            startActivity(intent);
+    }
+
+    public void daftar(View view){
+        Intent intent = new Intent(this, HalamanDaftar.class);
+        startActivity(intent);
+    }
+
+    public void fb(View view){
+        Uri uri = Uri.parse("https://web.facebook.com/rahadianirvan");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void gmail(View view){
+        Uri uri = Uri.parse("https://www.gmail.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    public void twitter(View view){
+        Uri uri = Uri.parse("https://www.twitter.com");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/rahadianirvan/mobpro1/HalamanMasuk.java
@@ -0,0 +1,13 @@
+package com.example.rahadianirvan.mobpro1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HalamanMasuk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_masuk);
+    }
+}
